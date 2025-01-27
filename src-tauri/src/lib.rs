4212df1f0905fda3479132ca82e3369b8c392585
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-01-27 13:30:51 +0900

    feat: add state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,31 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::sync::Mutex;
+
+use tauri::Manager;
+
+struct AppState {
+    keys: Vec<String>,
+}
+
 #[tauri::command]
-fn execute_command(command: &str) -> String {
-    format!("Command:{} executed", command)
+fn execute_command(state: tauri::State<Mutex<AppState>>, command: &str) -> String {
+    match command {
+        "ks" => {
+            let state = state.lock().unwrap();
+            state.keys.join(", ")
+        }
+        _ => "unknown command".to_string(),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            app.manage(Mutex::new(AppState {
+                keys: vec!["a".to_string(), "b".to_string()],
+            }));
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![execute_command])
         .run(tauri::generate_context!())
