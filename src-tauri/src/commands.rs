b45ec1f5fa7279c78eb31bab939e8fc904302bc6
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-01-28 15:33:21 +0900

    feat: add sql plugin + sample

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,24 +1,75 @@
-use std::sync::Mutex;
+use std::{sync::Mutex, time::SystemTime};
 
-use crate::state::AppState;
+use sqlx::{Executor, Row};
+use tauri_plugin_sql::{DbInstances, DbPool};
+
+use crate::{state::AppState, DB_URL};
 
 #[tauri::command]
-pub fn execute_command(state: tauri::State<Mutex<AppState>>, command: &str) -> String {
+pub async fn execute_command(
+    app_state: tauri::State<'_, Mutex<AppState>>,
+    db_instances: tauri::State<'_, DbInstances>,
+    command: &str,
+) -> Result<String, ()> {
     match command {
         "ks" => {
-            let state = state.lock().unwrap();
-            state.keys.join(", ")
+            let state = app_state.lock().unwrap();
+            Ok(state.keys.join(", "))
         }
         "p" => {
-            let state = state.lock().unwrap();
-            state.path.to_string_lossy().to_string()
+            let state = app_state.lock().unwrap();
+            Ok(state.path.to_string_lossy().to_string())
         }
         "f" => {
-            let state = state.lock().unwrap();
+            let state = app_state.lock().unwrap();
             let ret = std::fs::read_to_string(&state.path).unwrap();
             std::fs::write(&state.path, format!("{}\n{}", ret, "k3")).unwrap();
-            ret
+            Ok(ret)
         }
-        _ => "unknown command".to_string(),
+        "d" => {
+            let lock = db_instances.0.read().await;
+            let DbPool::Sqlite(db) = lock.get(DB_URL).unwrap();
+
+            let query = sqlx::query("SELECT * FROM keys");
+            let rows = db.fetch_all(query).await.unwrap();
+            let mut results = Vec::new();
+            rows.iter().for_each(|row| {
+                let item: &str = row.try_get("item").unwrap();
+                let username: &str = row.try_get("username").unwrap();
+                let key: &str = row.try_get("key").unwrap();
+                let created_at: i64 = row.try_get("created_at").unwrap();
+                let updated_at: i64 = row.try_get("updated_at").unwrap();
+                results.push(format!(
+                    "item: {}, username: {}, key: {}, created_at: {}, updated_at: {}",
+                    item, username, key, created_at, updated_at
+                ));
+            });
+            Ok(results.join("\n"))
+        }
+        "di" => {
+            let lock = db_instances.0.read().await;
+            let DbPool::Sqlite(db) = lock.get(DB_URL).unwrap();
+
+            let ts = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let query = sqlx::query(
+                r#"
+INSERT INTO keys ( item, username, key, created_at, updated_at )
+VALUES ( $1, $2, $3, $4, $5 )
+        "#,
+            )
+            .bind("item!")
+            .bind("username!")
+            .bind("key!")
+            .bind(ts as i64)
+            .bind(ts as i64);
+            let result = db.execute(query).await.unwrap();
+            println!("result: {:?}", result);
+
+            Ok("inserted".to_string())
+        }
+        _ => Ok("unknown command".to_string()),
     }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,10 +6,33 @@ use std::{fs, sync::Mutex};
 use state::AppState;
 use tauri::{path::BaseDirectory, Manager};
 use tauri_plugin_fs::FsExt;
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+pub(crate) const DB_URL: &str = "sqlite:pk.db";
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let db_migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_tables",
+        sql: r#"
+            CREATE TABLE IF NOT EXISTS keys (
+                item TEXT NOT NULL,
+                username TEXT NOT NULL,
+                key TEXT,
+                created_at INTEGER,
+                updated_at INTEGER,
+                PRIMARY KEY (item, username)
+            );"#,
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations(DB_URL, db_migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_fs::init())
         .setup(|app| {
             let base_path = app.path().resolve("", BaseDirectory::AppData).unwrap();
